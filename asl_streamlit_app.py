abfcd8f56046626b46442cb587e0ce3c353ce62d
dariadeev <dariadeev@gmail.com>
2025-06-22 10:46:15 +0300

    Update asl_streamlit_app.py

--- asl_streamlit_app.py
+++ asl_streamlit_app.py
@@ -54,7 +54,7 @@ class WNBModule(pl.LightningModule):
         pass
 
 # --- Model Path ---
-MODEL_STATE_DICT_PATH = "asl_resnet_model.pt"
+MODEL_STATE_DICT_PATH = "VGG19.pt"
 
 # --- Load Model ---
 @st.cache_resource
